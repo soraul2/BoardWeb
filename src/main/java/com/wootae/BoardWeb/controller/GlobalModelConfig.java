0b4a0264430f9eaecd31fc01e8a4eeaac29d83f3
kimtaewoo <goxodn1@gmail.com>
2025-12-24 17:19:20 +0900

    feat: 모든 페이지에 username,role,isLogin 데이터 Attribute 로 추가
    controlleradvice , ModelAttributes 사용

---
+++ src/main/java/com/wootae/BoardWeb/controller/GlobalModelConfig.java
@@ -0,0 +1,40 @@
+package com.wootae.BoardWeb.controller;
+
+import com.wootae.BoardWeb.dto.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.security.Principal;
+
+@ControllerAdvice
+public class GlobalModelConfig {
+
+    //@ModelAttributes 모든 컨트롤러가 실행되기 전에 이 메서드가 먼저 실행됨
+    @ModelAttribute
+    public void addAttributes(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        //로그인 한 상태인지 확인
+        if(auth != null && auth.isAuthenticated() && !"anonymousUser".equals(auth.getPrincipal())){
+            //principal 을 CustomUserDetails 로 변환
+            Object principal = auth.getPrincipal();
+            if(principal instanceof CustomUserDetails){
+                CustomUserDetails customUserDetails = (CustomUserDetails) principal;
+
+                //mustache 에서 쓸 변수명 등록
+                model.addAttribute("username",customUserDetails.getUsername());
+                model.addAttribute("role",customUserDetails.getAuthorities().toString());
+                model.addAttribute("isLogin",true);
+            }else{
+                model.addAttribute("isLogin",false);
+            }
+
+
+        }
+
+    }
+
+}
